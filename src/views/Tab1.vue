14363b0dbc61f13a7c2f19855988c3b3529f1dc4
Johanna0708 <schulzj@th-brandenburg.de>
2021-11-03 07:40:30 +0100

    Layout,Kamerafunktion

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -1,28 +1,28 @@
 <template>
   <ion-page>
     <ion-header>
-      <ion-toolbar>
-        <ion-subtitle>Pflanzenpflege</ion-subtitle>
+      <ion-toolbar class="ion-padding-top ion-padding-end">
+        <ion-item slot="end">
+          <div>
+            <img width="130" src="../img/Logo.png"/>
+          </div>
+        </ion-item>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
+
       <ion-title color="primary"><h2>Pflanzenpflege</h2></ion-title>
 
       <ion-card>
-      <ion-card-header>
-        <ion-card-title>Aufgaben</ion-card-title>
-        <ion-card-subtitle>in 5 Tagen</ion-card-subtitle>
-      </ion-card-header>
+        <ion-card-header>
+          <ion-card-title>Aufgaben</ion-card-title>
+          <ion-card-subtitle>in 5 Tagen</ion-card-subtitle>
+        </ion-card-header>
 
-      <ion-card-content>
-        Grünlilie muss gegossen werden!
-      </ion-card-content>
-        </ion-card>
+        <ion-card-content>
+          Grünlilie muss gegossen werden!
+        </ion-card-content>
+      </ion-card>
       <ion-card>
         <ion-card-header>
           <ion-card-subtitle>in 10 Tagen</ion-card-subtitle>
@@ -46,10 +46,10 @@
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon } from '@ionic/vue';
+import {IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonGrid, } from '@ionic/vue';
 
-export default  {
+export default {
   name: 'Tab1',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonIcon  }
+  components: {IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonIcon}
 }
 </script>
--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -1,28 +1,28 @@
 <template>
   <ion-page>
 
-    <ion-header>
-      <ion-item>
+    <!--<ion-header color="primary">
+     <ion-item>
         <div style="position: fixed; top:20px; right: 50px">
         <img width="130" src="../img/Logo.png"/>
       </div>
       </ion-item>
-    </ion-header>
+    </ion-header>-->
 
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="leaf" />
+          <ion-icon :icon="water" />
           <ion-label>Pflanzenpflege</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="image" />
-          <ion-label>Galerie</ion-label>
+        <ion-tab-button tab="tab2" href="/tabs/tab2">
+          <ion-icon :icon="leaf" />
+          <ion-label>Meine Pflanzen</ion-label>
         </ion-tab-button>
           
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
+        <ion-tab-button tab="tab3" href="/tabs/tab3">
           <ion-icon :icon="search" />
           <ion-label>Pflanzen suchen</ion-label>
         </ion-tab-button>
@@ -40,7 +40,7 @@
 
 <script lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet,  } from '@ionic/vue';
-import { leaf, folderOutline, image, settings, search } from 'ionicons/icons';
+import { leaf, folderOutline, image, settings, search,water } from 'ionicons/icons';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -53,7 +53,10 @@ export default defineComponent({
       image,
       settings,
       search,
+      water,
        }
   }
 });
+
+
 </script>
