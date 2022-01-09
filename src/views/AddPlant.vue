477885b9112877ee2a682fe33c9778e05b0eaf82
Johanna0708 <schulzj@th-brandenburg.de>
2022-01-09 22:12:20 +0100

    AddPlant erweitert

--- src/views/AddPlant.vue
+++ src/views/AddPlant.vue
@@ -15,27 +15,37 @@
       <ion-title color="primary"><h2>Pflanze hinzufügen</h2></ion-title>
 
 
-      <ion-item>
-          <ion-label>Pflanzenart</ion-label>
-          <ion-select placeholder="Select One">
-            <ion-select-option v-for="item in plants"
-                               v-bind:key="item.pid">{{ item.name }}</ion-select-option>
-          </ion-select>
-      </ion-item>
-      <ion-item>
+
+
+
+
+        <ion-label>Pflanzenart</ion-label>
+        <ion-select placeholder="Select One">
+          <ion-select-option v-for="item in plants"
+                             v-bind:key="item.pid">{{ item.name }}</ion-select-option>
+        </ion-select>
+
+
+        <ion-item>
         <ion-label>Sensor</ion-label>
-        <ion-input placeholder="Enter Input"></ion-input>
-      </ion-item>
-      <ion-item>
+        <ion-input v-model="senlink" placeholder="Enter Input"></ion-input>
 
-      <ion-router-link href="/:link/tabs/meinePflanzen">
-        <ion-button @click="sendData('save')">Speichern</ion-button>
-      </ion-router-link>
-
-      <ion-router-link href="/:link/tabs/meinePflanzen">
-        <ion-button>Abbrechen</ion-button>
-      </ion-router-link>
+        <!-- <ion-router-link href="/:link/tabs/meinePflanzen"> -->
+          <ion-button @click="sendData()">
+            Speichern
+          </ion-button>
+        <!-- </ion-router-link> -->
       </ion-item>
+
+
+
+
+
+
+        <ion-router-link href="/:link/tabs/meinePflanzen">
+          <ion-button>Abbrechen</ion-button>
+        </ion-router-link>
+
 
 
     </ion-content>
@@ -58,27 +68,28 @@ export default defineComponent ({
   data(){
     return{
       plants: null as unknown as Pflanze[],
-      sensordaten: null as any,
+      senlink: null as any,
 
     }
   },
+
   created() {
     axios.get('http://localhost:8080/planti/showall')
         .then(response => {
           this.plants = response.data
         })
   },
-
-  sendData() {
-    axios.get('http://localhost:8080/planti/addsensor')
-        .then(response => {
-          this.sensordaten = response.data
-          console.log(this.sensordaten)
-        })
-  }
-
-
-
+  methods: {
+    sendData(){
+      axios.get('http://localhost:8080/planti/addsensor', {params: {pid:10, link: this.senlink}})
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+  },
 })
 
 </script>
