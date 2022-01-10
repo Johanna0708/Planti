bd9840f681a4f44f7d52bf646053e3e54aab79fd
Johanna0708 <schulzj@th-brandenburg.de>
2022-01-10 15:25:21 +0100

    Exports ergänzt

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -11,29 +11,30 @@
     </ion-header>
 
     <ion-content fullscreen class="ion-padding">
-      <ion-title color="primary"><h2>Anmeldung</h2></ion-title>
+      <ion-title color="primary"><h2>{{ $t('login.title') }}</h2></ion-title>
 
       <ion-item>
-        <ion-label>Benutzername: </ion-label>
+        <ion-label>{{ $t('login.name') }}: </ion-label>
         <ion-input  v-model="username" placeholder="Enter Input"></ion-input>
       </ion-item>
       <ion-item>
-        <ion-label>Passwort: </ion-label>
+        <ion-label>{{ $t('login.password') }}: </ion-label>
         <ion-input v-model="pw" type="password" placeholder="Enter Input"></ion-input>
       </ion-item>
 
 
-      <ion-button v-on:click="anmelden">Anmelden</ion-button>
+
+      <ion-button v-on:click="anmelden">{{ $t('login.button1') }}</ion-button>
 
       <ion-router-link href="/:link/tabs/tab3">
-        <ion-button>Abbrechen</ion-button>
+        <ion-button>{{ $t('login.button2') }}</ion-button>
       </ion-router-link>
 
 
       <ion-router-link href="/:link/tabs/reg">
       <ion-item class="ion-padding-top">
-        Neu bei Planti?
-        <button> Neuen Account anlegen</button>
+        {{ $t('login.newAcc') }}
+        <button> {{ $t('login.button3') }}</button>
       </ion-item>
       </ion-router-link>
     </ion-content>
@@ -42,7 +43,7 @@
 
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonItem, IonLabel, IonButton } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import axios from "axios";
 import router from "@/router";
@@ -51,7 +52,7 @@ import router from "@/router";
 
 export default defineComponent ({
   name: 'Login',
-  components: {IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonInput},
+  components: {IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonInput, IonItem, IonLabel, IonButton},
 
   data(){
     return{
