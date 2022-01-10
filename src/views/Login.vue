3840c1a5d0234a4983b4ed11e59d100db147dada
THBSimon <simon.wichura@th-brandenburg.de>
2022-01-10 17:51:37 +0100

    Login Schritt 3: Finale Implementation des Logins, insbesondere die Registrierung. Bugfixing.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -11,30 +11,29 @@
     </ion-header>
 
     <ion-content fullscreen class="ion-padding">
-      <ion-title color="primary"><h2>{{ $t('login.title') }}</h2></ion-title>
+      <ion-title color="primary"><h2>Anmeldung</h2></ion-title>
 
       <ion-item>
-        <ion-label>{{ $t('login.name') }}: </ion-label>
+        <ion-label>Benutzername: </ion-label>
         <ion-input  v-model="username" placeholder="Enter Input"></ion-input>
       </ion-item>
       <ion-item>
-        <ion-label>{{ $t('login.password') }}: </ion-label>
+        <ion-label>Passwort: </ion-label>
         <ion-input v-model="pw" type="password" placeholder="Enter Input"></ion-input>
       </ion-item>
 
-      <!--<ion-router-link href="/:link/tabs/meinePflanzen">-->
-      <ion-button v-on:click="anmelden">{{ $t('login.button1') }}</ion-button>
-      <!--</ion-router-link>-->
+
+      <ion-button v-on:click="anmelden">Anmelden</ion-button>
 
       <ion-router-link href="/:link/tabs/tab3">
-        <ion-button>{{ $t('login.button2') }}</ion-button>
+        <ion-button>Abbrechen</ion-button>
       </ion-router-link>
 
 
       <ion-router-link href="/:link/tabs/reg">
       <ion-item class="ion-padding-top">
-        {{ $t('login.newAcc') }}
-        <button> {{ $t('login.button3') }}</button>
+        Neu bei Planti?
+        <button> Neuen Account anlegen</button>
       </ion-item>
       </ion-router-link>
     </ion-content>
@@ -43,17 +42,18 @@
 
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonItem, IonLabel, IonButton } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import axios from "axios";
+import router from "@/router";
 
 
 
 export default defineComponent ({
   name: 'Login',
-  components: {IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonInput, IonItem, IonLabel, IonButton},
+  components: {IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonInput},
 
-  data: function(){
+  data(){
     return{
       username:"",
       pw:""
@@ -68,6 +68,8 @@ export default defineComponent ({
       .then(response => {
         if (response.data==this.pw){
           window.localStorage.setItem("username", this.username)
+          //location.reload()
+          router.push("/:link/tabs/meinePflanzen")
         }
         else {
           alert("Passwort oder Nutzername ist falsch.")
@@ -75,17 +77,11 @@ export default defineComponent ({
       })
       console.log(window.localStorage.getItem("username"))
     }
-
   },
   created(){
 
     console.log(window.localStorage.getItem("username"))
   }
-
-
-
-
-
 })
 
 </script>
--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -11,20 +11,20 @@
     </ion-header>
 
     <ion-content fullscreen class="ion-padding">
-      <ion-title color="primary"><h2>{{ $t('reg.title') }}</h2></ion-title>
+      <ion-title color="primary"><h2>Registrierung</h2></ion-title>
 
       <ion-item>
-        <ion-label>{{ $t('reg.name') }} </ion-label>
-        <ion-input placeholder="Enter Input"></ion-input>
+        <ion-label>Benutzername: </ion-label>
+        <ion-input  v-model="username" placeholder="Enter Input"></ion-input>
       </ion-item>
       <ion-item>
-        <ion-label>{{ $t('reg.password') }} </ion-label>
-        <ion-input placeholder="Enter Input"></ion-input>
+        <ion-label>Passwort: </ion-label>
+        <ion-input v-model="pw" type="password" placeholder="Enter Input"></ion-input>
       </ion-item>
 
-      <ion-button>{{ $t('reg.button1') }} </ion-button>
+      <ion-button v-on:click="registrieren">Konto anlegen</ion-button>
       <ion-router-link href="/:link/tabs/tab3">
-        <ion-button>{{ $t('reg.button2') }} </ion-button>
+        <ion-button>Abbrechen</ion-button>
       </ion-router-link>
 
     </ion-content>
@@ -33,17 +33,42 @@
 
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonButton } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput} from '@ionic/vue';
 import { defineComponent } from 'vue';
+import axios from "axios";
+import router from "@/router";
 
 
 
 export default defineComponent ({
   name: 'Registration',
-  components: {IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonItem, IonLabel, IonInput, IonButton},
-
-
+  components: {IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonInput},
+  data(){
+    return{
+      username:"",
+      pw:""
+    }
+  },
+  methods: {
+      async registrieren() {
+          await axios.get("http://localhost:8080/planti/adduser", {params: {username: this.username, pw: this.pw}})
+                    .then(() => {
+                      window.localStorage.setItem("username", this.username)
+                      //location.reload()
+                      router.push("/:link/tabs/meinePflanzen")
+                    })
+                    .catch(function (error) {
+                      if (error.response) {
+                        alert("Dieser Benutzername ist bereits vergeben!")
+                      } else {
+                        alert("Ein Fehler ist aufgetreten. Bitte versuchen Sie es erneut.")
+                      }
+                    })
+    }
+  }
 })
+
+
 
 </script>
 
