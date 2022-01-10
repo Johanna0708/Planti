d65db7693dc82ee95fa8213be4a4fd6769f610f6
Johanna0708 <schulzj@th-brandenburg.de>
2022-01-10 15:25:21 +0100

    Exports ergänzt

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -11,20 +11,20 @@
     </ion-header>
 
     <ion-content fullscreen class="ion-padding">
-      <ion-title color="primary"><h2>Registrierung</h2></ion-title>
+      <ion-title color="primary"><h2>{{ $t('reg.title') }}</h2></ion-title>
 
       <ion-item>
-        <ion-label>Benutzername: </ion-label>
+        <ion-label>{{ $t('reg.name') }} </ion-label>
         <ion-input  v-model="username" placeholder="Enter Input"></ion-input>
       </ion-item>
       <ion-item>
-        <ion-label>Passwort: </ion-label>
+        <ion-label>{{ $t('reg.password') }} </ion-label>
         <ion-input v-model="pw" type="password" placeholder="Enter Input"></ion-input>
       </ion-item>
 
-      <ion-button v-on:click="registrieren">Konto anlegen</ion-button>
+      <ion-button v-on:click="registrieren">{{ $t('reg.button1') }} </ion-button>
       <ion-router-link href="/:link/tabs/tab3">
-        <ion-button>Abbrechen</ion-button>
+        <ion-button>{{ $t('reg.button2') }} </ion-button>
       </ion-router-link>
 
     </ion-content>
@@ -33,7 +33,7 @@
 
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput} from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonButton } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import axios from "axios";
 import router from "@/router";
@@ -42,7 +42,7 @@ import router from "@/router";
 
 export default defineComponent ({
   name: 'Registration',
-  components: {IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonInput},
+  components: {IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonItem, IonLabel, IonInput, IonButton},
   data(){
     return{
       username:"",
@@ -67,8 +67,6 @@ export default defineComponent ({
     }
   }
 })
-
-
 
 </script>
 
